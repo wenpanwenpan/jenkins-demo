f54855cd22728092a9868a0680f83d8f27ff6743
wenpan <869485274@qq.com>
2019-09-29 17:44:05 +0800

    [ADD] create by wenpan

---
+++ src/main/java/com/wp/demo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.wp.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * description
+ *
+ * @author 文攀 2019/09/29 17:35
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String hello(){
+        System.out.println("============>>>  test jenkins !!!!");
+        return "hello";
+    }
+}
