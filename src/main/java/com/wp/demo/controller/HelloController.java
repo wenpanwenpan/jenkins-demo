4f0cb802eb631fba0cac639f033a365e61270ecf
wenpan <869485274@qq.com>
2019-09-30 15:35:24 +0800

    测试2

--- src/main/java/com/wp/demo/controller/HelloController.java
+++ src/main/java/com/wp/demo/controller/HelloController.java
@@ -2,18 +2,21 @@ package com.wp.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * description
  *
  * @author 文攀 2019/09/29 17:35
  */
-@Controller
+@RestController
 public class HelloController {
 
     @GetMapping(value = "/hello")
+    @ResponseBody
     public String hello(){
         System.out.println("============>>>  test jenkins !!!!");
-        return "hello wenpan！！！";
+        return "hello";
     }
 }
