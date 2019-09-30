91d10b39acd434c014ca15c72a94c4860a71009d
wenpan <869485274@qq.com>
2019-09-30 16:47:18 +0800

    测试4

--- src/main/java/com/wp/demo/SpringBootStartApplication.java
+++ src/main/java/com/wp/demo/SpringBootStartApplication.java
@@ -9,11 +9,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @author 文攀 2019/09/30 15:53
  */
 
-public class SpringBootStartApplication extends SpringBootServletInitializer {
+/*public class SpringBootStartApplication extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         // 注意这里要指向原先用main方法执行的Application启动类
         return builder.sources(JenkinsDemoApplication.class);
     }
-}
+}*/
